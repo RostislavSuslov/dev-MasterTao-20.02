a9af9129a172689aa34e0210d8a203b54752149f
RostSuslov <suslovros@gmail.com>
2024-05-17 21:27:06 +0300

    test: install sliders

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,10 +1,36 @@
-const burger = document.querySelector('#burger');
+const burger = document.querySelector('.hamburger');
 const mobileContainer = document.querySelector('.mobile-container');
 
 
 function showBurgerMenu() {
     mobileContainer.classList.toggle('show')
-    burger.classList.toggle('active')
+    burger.classList.toggle('is-active')
 }
 
 burger.addEventListener('click', showBurgerMenu)
+
+
+const swiper = new Swiper('.swiper', {  
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 1000,
+      },
+});
+
+$(document).ready(function(){
+    $('.your-class').slick({
+        dots: true,
+        infinite: false,
+        pauseOnHover: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+    });
+});
