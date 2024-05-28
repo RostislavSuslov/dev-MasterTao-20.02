4fb183d574918005292dc84287d508384ab38337
RostSuslov <suslovros@gmail.com>
2024-05-28 20:05:29 +0300

    feat: first-screen done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,29 +1,39 @@
 const burger = document.querySelector('.hamburger');
+const body = document.querySelector('body');
 const mobileContainer = document.querySelector('.mobile-container');
 
 
 function showBurgerMenu() {
     mobileContainer.classList.toggle('show')
     burger.classList.toggle('is-active')
+    body.classList.toggle('overflow-hidden')
 }
 
 burger.addEventListener('click', showBurgerMenu)
 
 
 const swiper = new Swiper('.slider-info', {  
-    spaceBetween: 40,
+    spaceBetween: 12,
     loop: true,
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
     },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+   
+    navigation:  {
+      enabled: false
     },
-    // autoplay: {
-    //     delay: 5000,
-    //   },
+
+    breakpoints: {
+      992: {
+        spaceBetween: 40,
+        navigation: {
+          enabled: true,
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      }
+    }
 });
 
 // $(document).ready(function(){
