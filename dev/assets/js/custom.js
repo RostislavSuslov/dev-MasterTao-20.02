56b2d5eb16cfe3e795d0b67e34ec3fb5043da1c9
RostSuslov <suslovros@gmail.com>
2024-06-07 21:34:00 +0300

    feat: slider advantages

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -12,27 +12,27 @@ function showBurgerMenu() {
 burger.addEventListener('click', showBurgerMenu)
 
 
-const swiper = new Swiper('.slider-info', {  
+const swiperInfo = new Swiper('.slider-info', {
     spaceBetween: 12,
     loop: true,
     pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+        el: '.swiper-pagination',
+        clickable: true,
     },
-   
-    navigation:  {
-      enabled: false
+
+    navigation: {
+        enabled: false
     },
 
     breakpoints: {
-      992: {
-        spaceBetween: 40,
-        navigation: {
-          enabled: true,
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      }
+        1025: {
+            spaceBetween: 40,
+            navigation: {
+                enabled: true,
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        }
     }
 });
 
@@ -41,14 +41,69 @@ const fileLable = document.querySelectorAll('.calculation-label');
 const fileInput = document.querySelectorAll('.calculation-file');
 
 fileInput.forEach(item => {
-  item.addEventListener('change', function() {
-    let fileName = item.closest('.input-box').querySelector('.file-name')
-    fileName.innerHTML  = item.files[0].name;
-  })
+    item.addEventListener('change', function() {
+        let fileName = item.closest('.input-box').querySelector('.file-name')
+        fileName.innerHTML = item.files[0].name;
+    })
 })
 
-fileLable.forEach(item => item.closest('.form-row').previousElementSibling.classList.add('special-gap') )
- 
+fileLable.forEach(item => item.closest('.form-row').previousElementSibling.classList.add('special-gap'))
+
+
+/*swiperAdvantages*/
+let swiperAdvantages  
+
+function initAdvantages() {
+    swiperAdvantages = new Swiper('.slider-advantages', {
+    spaceBetween: 12,
+    loop: true,
+  
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        enabled: true,
+    },
+    breakpoints: {
+        1025: {
+            spaceBetween: 40,
+            navigation: {
+                enabled: true,
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            pagination: {
+                enabled: false,
+            },
+  
+        }
+    }
+  });
+}
+
+function destroyAdvantages() {
+  swiperAdvantages.destroy( true, true )   
+}
+
+
+function mediaQuery(matchMedia1024) {
+    if (matchMedia1024.matches) { // If media query matches
+      initAdvantages()
+    } else {
+      destroyAdvantages()
+    }
+}
+
+// Create a MediaQueryList object
+const matchMedia1024 = window.matchMedia("(max-width: 1024px)")
+
+// Call listener function at run time
+mediaQuery(matchMedia1024);
+
+// Attach listener function on state changes
+matchMedia1024.addEventListener("change", function() {
+    mediaQuery(matchMedia1024);
+});
+/**/
 
 
 
@@ -59,15 +114,6 @@ fileLable.forEach(item => item.closest('.form-row').previousElementSibling.class
 
 
 
-
-
-
-
-
-
-
-
- 
 // $(document).ready(function(){
- 
+
 // });
