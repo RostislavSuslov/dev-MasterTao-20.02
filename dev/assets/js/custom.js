cecf5f3933494e2d3e36f567c5b0f92bd16cf401
RostSuslov <suslovros@gmail.com>
2024-06-11 21:40:44 +0300

    feat: slider-template desktop done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -37,6 +37,7 @@ const swiperInfo = new Swiper('.slider-info', {
 });
 
 
+
 const fileLable = document.querySelectorAll('.calculation-label');
 const fileInput = document.querySelectorAll('.calculation-file');
 
@@ -51,21 +52,77 @@ fileLable.forEach(item => item.closest('.form-row').previousElementSibling.class
 
 
 /*swiperAdvantages*/
-let swiperAdvantages  
+// let swiperAdvantages  
 
-function initAdvantages() {
-    swiperAdvantages = new Swiper('.slider-advantages', {
-    spaceBetween: 12,
+// function initAdvantages() {
+//     swiperAdvantages = new Swiper('.slider-advantages', {
+//     spaceBetween: 12,
+//     loop: true,
+
+//     pagination: {
+//         el: '.swiper-pagination',
+//         clickable: true,
+//         enabled: true,
+//     },
+//     breakpoints: {
+//         1025: {
+//             spaceBetween: 40,
+//             navigation: {
+//                 enabled: true,
+//                 nextEl: '.swiper-button-next',
+//                 prevEl: '.swiper-button-prev',
+//             },
+//             pagination: {
+//                 enabled: false,
+//             },
+
+//         }
+//     }
+//   });
+// }
+
+// function destroyAdvantages() {
+//   swiperAdvantages.destroy( true, true )   
+// }
+
+
+// function mediaQuery(matchMedia1024) {
+//     if (matchMedia1024.matches) { // If media query matches
+//       initAdvantages()
+//     } else {
+//       destroyAdvantages()
+//     }
+// }
+
+// // Create a MediaQueryList object
+// const matchMedia1024 = window.matchMedia("(max-width: 1024px)")
+
+// // Call listener function at run time
+// mediaQuery(matchMedia1024);
+
+// // Attach listener function on state changes
+// matchMedia1024.addEventListener("change", function() {
+//     mediaQuery(matchMedia1024);
+// });
+/**/
+
+const swiperTemplate = new Swiper('.swiper-template', {
+    speed: 700,
     loop: true,
-  
+    slidesPerView: 1,
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
-        enabled: true,
     },
+
+    navigation: {
+        enabled: false
+    },
+
     breakpoints: {
         1025: {
-            spaceBetween: 40,
+            slidesPerView: 3,
+            spaceBetween: 20,
             navigation: {
                 enabled: true,
                 nextEl: '.swiper-button-next',
@@ -74,37 +131,9 @@ function initAdvantages() {
             pagination: {
                 enabled: false,
             },
-  
         }
     }
-  });
-}
-
-function destroyAdvantages() {
-  swiperAdvantages.destroy( true, true )   
-}
-
-
-function mediaQuery(matchMedia1024) {
-    if (matchMedia1024.matches) { // If media query matches
-      initAdvantages()
-    } else {
-      destroyAdvantages()
-    }
-}
-
-// Create a MediaQueryList object
-const matchMedia1024 = window.matchMedia("(max-width: 1024px)")
-
-// Call listener function at run time
-mediaQuery(matchMedia1024);
-
-// Attach listener function on state changes
-matchMedia1024.addEventListener("change", function() {
-    mediaQuery(matchMedia1024);
 });
-/**/
-
 
 
 
